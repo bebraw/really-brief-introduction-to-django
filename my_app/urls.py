1829980397f9b0c940b1fd4694e76dfd7b27610a
Juho Vepsalainen <bebraw@gmail.com>
2012-02-08 15:32:34 +0200

    Set up ModelForm example

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import patterns
 import views
 
 urlpatterns = patterns('',
+    (r'^create_person/$', views.create_person),
     (r'^hello/$', views.hello),
     (r'^hello/(?P<id>\d+)/$', views.hello2)
 )
--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,7 @@
+from django.core.context_processors import csrf
 from django.shortcuts import get_object_or_404, render_to_response
 import models
+from forms import PersonForm
 
 def hello(request):
     return render_to_response('my_app/hello.html',
@@ -12,4 +14,15 @@ def hello2(request, id=None):
     return render_to_response('my_app/hello.html',
         {'target': target} # fails silently for names by default
     )
+
+def create_person(request):
+    form = PersonForm()
+    if request.method == 'POST':
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    c = {'form': form}
+    c.update(csrf(request))
+    return render_to_response('my_app/create.html', c)
 
