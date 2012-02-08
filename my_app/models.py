24b77a81fa83fcfc3d05fa11caf4adea614e987e
Juho Vepsalainen <bebraw@gmail.com>
2012-02-08 15:06:30 +0200

    Set up initial model

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.name
+
